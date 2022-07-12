98d20094f8f6e7b527bfb533cf06a68d8df71e4c
grsLammy <grspro98@gmail.com>
2022-07-12 15:29:13 +0530

    feat: add interface

---
+++ src/Interface/IDynamicCounterTest.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+interface IDynamicCounterTest {
+    function increaseCount(uint256 _incrementAmount) external;
+
+    function getTotalCount() external view returns (uint256);
+
+    function getLastExecuted() external view returns (uint256);
+}
