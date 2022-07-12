12dfb043cf229966fc88af416e8a3250ae6674da
grsLammy <grspro98@gmail.com>
2022-07-12 11:24:27 +0530

    feat: initial commit

---
+++ hardhat.config.ts
@@ -0,0 +1,49 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-waffle";
+import dotenv from "dotenv";
+import "hardhat-deploy";
+import "solidity-coverage";
+import "@typechain/hardhat";
+import "hardhat-gas-reporter";
+import "@nomiclabs/hardhat-etherscan";
+dotenv.config();
+
+const config: HardhatUserConfig = {
+    solidity: "0.8.4",
+    networks: {
+        polygon: {
+            url: "https://polygon-rpc.com/",
+            gasPrice: "auto", // if txs failing, set manual fast gas price
+            accounts: process.env.PRIVATE_KEY_POLYGON !== undefined ? [process.env.PRIVATE_KEY_POLYGON] : [],
+        },
+        mumbai: {
+            url: `https://polygon-mumbai.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+            gasPrice: "auto",
+            accounts: process.env.PRIVATE_KEY_POLYGON !== undefined ? [process.env.PRIVATE_KEY_POLYGON] : [],
+        },
+    },
+    namedAccounts: {
+        deployer: 0,
+        admin: 1,
+        admin1: 2,
+        user: 3,
+        user1: 4,
+    },
+    paths: {
+        sources: "src",
+    },
+    typechain: {
+        outDir: "src/types",
+        target: "ethers-v5",
+    },
+    gasReporter: {
+        enabled: process.env.REPORT_GAS !== undefined,
+        currency: "USD",
+    },
+    etherscan: {
+        apiKey: process.env.EXPLORER_API_KEY || "",
+    },
+};
+
+export default config;
